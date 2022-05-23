30c37ed1b621268085f2894e585c52ecfaf89b0f
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2022-05-23 23:49:42 +0900

    html_with_py-script
    drill for py-script and html

---
+++ exercise1/my_models.py
@@ -0,0 +1,10 @@
+import bokeh
+import matplotlib as plt
+import pandas as pd
+import numpy as np
+#import scikit-learn
+
+def make_a_y(n):
+    x = np.random.randn(n)
+    y = np.random.randn(n)
+    return x, y
